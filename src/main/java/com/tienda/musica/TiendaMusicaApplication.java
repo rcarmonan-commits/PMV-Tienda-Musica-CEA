abefa575949d77559157ae155f3f5c2f7cd0d267
rcarmonan-commits <rcarmonan@unicartagena.edu.co>
2026-04-08 22:28:02 -0500

    Create TiendaMusicaApplication.java

---
+++ src/main/java/com/tienda/musica/TiendaMusicaApplication.java
@@ -0,0 +1,11 @@
+package com.tienda.musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMusicaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaMusicaApplication.class, args);
+    }
+}
