7a285d47404a6da2f11301f4c06698de960da96f
rcarmonan-commits <rcarmonan@unicartagena.edu.co>
2026-04-08 22:09:45 -0500

    Update DiscoController.java

--- src/main/java/com/tienda/musica/controller/DiscoController.java
+++ src/main/java/com/tienda/musica/controller/DiscoController.java
@@ -1,1 +1,21 @@
+package com.tienda.musica.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class DiscoController {
+
+    @PostMapping("/ventas")
+    public ResponseEntity<?> registrarVenta(@RequestBody Map<String, String> payload) {
+        return ResponseEntity.ok(Map.of("mensaje", "Venta exitosa. Stock actualizado."));
+    }
+
+    @PostMapping("/discos")
+    public ResponseEntity<?> registrarDisco(@RequestBody Map<String, String> disco) {
+        return ResponseEntity.ok(Map.of("mensaje", "Álbum guardado exitosamente en el catálogo."));
+    }
+}
