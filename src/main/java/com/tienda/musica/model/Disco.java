5eaab1fa18f26e61fa5b3a4d91c57c5a674ad829
rcarmonan-commits <rcarmonan@unicartagena.edu.co>
2026-04-08 22:46:50 -0500

    Update Disco.java

--- src/main/java/com/tienda/musica/model/Disco.java
+++ src/main/java/com/tienda/musica/model/Disco.java
@@ -1,5 +1,4 @@
 package com.tienda.musica.model;
-
 import jakarta.persistence.*;
 
 @Entity
@@ -8,12 +7,9 @@ public class Disco {
     private String upc;
     private String titulo;
     private int stock;
-
     // Getters y Setters
     public String getUpc() { return upc; }
-    public void setUpc(String upc) { this.upc = upc; }
     public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
     public int getStock() { return stock; }
     public void setStock(int stock) { this.stock = stock; }
 }
