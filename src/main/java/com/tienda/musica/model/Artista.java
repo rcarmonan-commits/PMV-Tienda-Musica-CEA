accb65092ceeed0fd03f767c347ea98063ae20a5
rcarmonan-commits <rcarmonan@unicartagena.edu.co>
2026-04-08 22:46:26 -0500

    Update Artista.java

--- src/main/java/com/tienda/musica/model/Artista.java
+++ src/main/java/com/tienda/musica/model/Artista.java
@@ -1,26 +1,14 @@
 package com.tienda.musica.model;
-
 import jakarta.persistence.*;
 
 @Entity
 public class Artista {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String tipo; // Solista o Grupo
+    private String tipo;
     private String nombre;
     private String pais;
-    private String integrantes;
-
     // Getters y Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getTipo() { return tipo; }
-    public void setTipo(String tipo) { this.tipo = tipo; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getPais() { return pais; }
-    public void setPais(String pais) { this.pais = pais; }
-    public String getIntegrantes() { return integrantes; }
-    public void setIntegrantes(String integrantes) { this.integrantes = integrantes; }
 }
